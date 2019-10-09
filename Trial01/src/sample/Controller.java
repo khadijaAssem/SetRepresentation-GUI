16dd87a325b5388352a6d900225d7c1f614f77e5
khadijaAssem <42361430+khadijaAssem@users.noreply.github.com>
2019-10-09 22:51:41 +0200

    Last GUI

--- Trial01/src/sample/Controller.java
+++ Trial01/src/sample/Controller.java
@@ -1,35 +1,108 @@
 package sample;
 
-import javafx.event.ActionEvent;
-import java.awt.*;
 import java.util.*;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import sun.awt.Win32ColorModel24;
 
 public class Controller {
     @FXML
-    public static TextField universal;
+    private TextField universe;
     @FXML
-    public static Pane screen;
+    private TextField addSubset;
     @FXML
-    public static VBox pane_main_grid;
-    public static void findSets(){
-//        Scanner input = new Scanner(System.in);
-//        String Un = input.nextLine();
-        String[] Universal = universal.getText().split("\\s");
-        for(String w:Universal)
-            System.out.println(w);
-//        String a = input.nextLine();
-//        String[] A = a.split("\\s");
-//        A = removeDuplicates(A);
-//        for(String w:Universal)
-//            System.out.println(w);
-//        System.out.println(intersection(Universal,A));
+    private Label subsets;
+    @FXML
+    private Label Error;
+    @FXML
+    private TextField operation;
+    @FXML
+    private Label answer;
+    @FXML
+    private Pane screen;
+
+    static ArrayList<String []> sets = new ArrayList<String []>();
+    String[] Universal;
+    char setName = 'A';
+
+    @FXML
+    public void findSets(){
+        LinkedList<Circle> circles = new LinkedList<>();
+        for (int i=0 ;i<sets.size();i++){
+            Circle Ccls = new Circle();
+
+        }
+        Universal= universe.getText().split(",");
+        String[] newSet =(addSubset.getText().split(","));
+        boolean v = validsubset(newSet,Universal);
+        while(!v){
+            Error.setText("Invalid subset!");
+            addSubset.setText("");
+            return;
+        }
+        Error.setText("");
+        sets.add(setName-65,newSet);
+        subsets.setText(subsets.getText()+"\n"+ setName +" : "+addSubset.getText());
+        setName++;
+        addSubset.setText("");
     }
-    public static LinkedList<String> intersection (String[] set1, String[] set2){
-        LinkedList<String> Intersection = new LinkedList<>();
+    @FXML
+    public void operation(){
+        String statement = operation.getText();
+        String[] Stat = (statement.split("\\s"));
+        operation.setText("");
+        if (Stat[1].toUpperCase().equals("I")) {
+            String[] A = sets.get(Stat[0].charAt(0) - 65);
+            A = removeDuplicates(A);
+            String[] B = sets.get(Stat[2].charAt(0) - 65);
+            B = removeDuplicates(B);
+            if (intersection(A, B).size()==0)
+                answer.setText(statement + " = " +"PHI");
+            else
+                answer.setText(statement + " = " + intersection(A, B));
+        } else if (Stat[1].toUpperCase().equals("U")) {
+            String[] A = sets.get(Stat[0].charAt(0) - 65);
+            A = removeDuplicates(A);
+            String[] B = sets.get(Stat[2].charAt(0) - 65);
+            B = removeDuplicates(B);
+            answer.setText(statement + " = " +Union(A, B));
+        } else if (Stat[1].toUpperCase().equals("C")) {
+            String[] A = sets.get(Stat[0].charAt(0) - 65);
+            A = removeDuplicates(A);
+            if (complement(Universal, A).size()==0)
+                answer.setText(statement + " = " +"PHI");
+            else
+                answer.setText(statement + " = " +complement(Universal, A));
+        }
+    }
+    public static boolean validsubset(String[]set1,String[]set2){
+        int found =0;
+        for(int i=0;i<set1.length;i++){
+            for(int j=0;j<set2.length;j++){
+                if (set1[i].equals(set2[j])){
+                    found++;
+                    break;
+                }
+            }
+        }
+        if(found==set1.length)
+            return true;
+        else
+            return false;
+    }
+    public static LinkedList<String> intersection (String[] set1,String[] set2){
+        LinkedList<String> Intersection = new LinkedList<String>();
         for (int i=0;i<set1.length;i++){
             for (int j=0;j<set2.length;j++){
                 if (set1[i].equals(set2[j])){
@@ -41,9 +114,25 @@ public class Controller {
         Intersection = RemoveDuplicates(Intersection);
         return Intersection;
     }
-
+    public static LinkedList<String> complement (String []universal,String[] set1){
+        LinkedList<String> complenemt = new LinkedList<String>();
+        int found;
+        for (int i=0;i<universal.length;i++){
+            found=0;
+            for (int j=0;j<set1.length;j++){
+                if (universal[i].equals(set1[j])){
+                    break;
+                }
+                found++;
+            }
+            if(found==set1.length)
+                complenemt.add(universal[i]);
+        }
+        complenemt = RemoveDuplicates(complenemt);
+        return complenemt;
+    }
     public static LinkedList<String> Union (String[] set1, String[] set2){
-        LinkedList<String> Union = new LinkedList<>();
+        LinkedList<String> Union = new LinkedList<String>();
         for (int i=0;i<set1.length;i++)
             Union.add(set1[i]);
         for (int i=0;i<set2.length;i++)
@@ -51,7 +140,6 @@ public class Controller {
         Union = RemoveDuplicates(Union);
         return Union;
     }
-
     public static String[] removeDuplicates(String[] list){
         Set set = new LinkedHashSet();// Create a new LinkedHashSet
         set.addAll(Arrays.asList(list));// Add the elements to set
@@ -61,7 +149,6 @@ public class Controller {
             List[i++] = (String) W;// add the elements of set with no duplicates to the list
         return list;
     }
-
     public static LinkedList RemoveDuplicates(LinkedList list){
         Set set = new LinkedHashSet();// Create a new LinkedHashSet
         set.addAll(list);// Add the elements to set
@@ -69,19 +156,20 @@ public class Controller {
         list.addAll(set);// add the elements of set with no duplicates to the list
         return list;
     }
-
-    static String name = "set";
-    static int countSet = 0;
-    @FXML
-    public void newSet(ActionEvent actionEvent){
-        name = name + countSet ;
-        TextField name = new TextField();
-//        TextField textField[] = new TextField[15];
-//        textField[countSet] = new TextField();
-//        root.add(textField[countSet], 5, countSet);
-        TextField newField = new TextField();
-        newField.setMinWidth(120);
-
-        pane_main_grid.getChildren().add(newField);
+    public static LinkedList<String> Difference (String []set1,String[] set2){
+        LinkedList<String> difference = new LinkedList<String>();
+        int found;
+        for (int i=0;i<set1.length;i++){
+            found=0;
+            for (int j=0;j<set2.length;j++){
+                if (set1[i].equals(set2[j])){
+                    found=1;
+                }
+            }
+            if(found==0)
+                difference.add(set1[i]);
+        }
+        difference = RemoveDuplicates(difference);
+        return difference;
     }
 }
--- Trial01/src/sample/Main.java
+++ Trial01/src/sample/Main.java
@@ -1,40 +1,20 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.awt.*;
-import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-
 public class Main extends Application {
-    @FXML
-    public static Button addSet;
-    int i=0;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-//        TextField textField[] = new TextField[15];
-//        addSet.setOnAction(e -> {
-//            textField[i] = new TextField();
-//            root.  (textField[i], 5, i);
-//            i = i + 1;
-//
-//        });
-        primaryStage.setScene(new Scene(root, 300, 275));
-
+        primaryStage.setScene(new Scene(root, 683, 425));
+        primaryStage.setTitle("Set Operations");
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
